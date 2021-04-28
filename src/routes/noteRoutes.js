f96fd7d780464327cd1e658f43d83f385c57aa3a
Rizqon Maulana <rizqonmaulana5@gmail.com>
2021-04-28 08:56:24 +0800

    add note route form validation

--- src/routes/noteRoutes.js
+++ src/routes/noteRoutes.js
@@ -2,12 +2,20 @@
 const express = require("express");
 const note = require("../controllers/noteController");
 const router = express.Router();
+const { validationResult } = require("express-validator");
+const noteValidation = require("./validation/noteValidation");
 
 router.get(`/api/note`, note.index);
 router.get("/api/note/user/:id", note.getByUserId);
-router.post(`/api/note`, note.store);
 router.get(`/api/note/:id`, note.show);
-router.put(`/api/note/:id`, note.update);
+router.post(`/api/note`, [noteValidation.form], (req, res) => {
+  const errors = validationResult(req);
+  !errors.isEmpty() ? res.status(422).json(errors) : note.store(req, res);
+});
+router.put(`/api/note/:id`, [noteValidation.form], (req, res) => {
+  const errors = validationResult(req);
+  !errors.isEmpty() ? res.status(422).json(errors) : note.update(req, res);
+});
 router.delete(`/api/note/:id`, note.destroy);
 
 module.exports = router;
